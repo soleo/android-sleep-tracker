cdabdc87d6ac95f07db343db168016e894a5352e
Xiang Huo <huoxiang5659@gmail.com>
2013-04-25 10:52:16 -0500

    Revert "Revert "Alarm-Demo-3""
    This reverts commit d4d682b92602feab102a549d3693028b54fb29ad.

--- Yiting/alarm-demo/gen/com/example/alarm_demo/R.java
+++ Yiting/alarm-demo/gen/com/example/alarm_demo/R.java
@@ -24,16 +24,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
+        public static final int action_settings=0x7f080007;
         public static final int btnRecordAlarm=0x7f080005;
         public static final int btnSelectAlarm=0x7f080004;
         public static final int btnSetAlarm1=0x7f080003;
         public static final int btnSetAlarm2=0x7f080002;
         public static final int btnStopAlarm=0x7f080001;
+        public static final int stopRecordBtn=0x7f080006;
         public static final int txtSeconds=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int recording=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
